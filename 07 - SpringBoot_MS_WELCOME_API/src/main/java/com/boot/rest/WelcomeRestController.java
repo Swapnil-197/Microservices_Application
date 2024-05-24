cf68101bc1ccc1fb8df42e7b944e5e0e92483e68
Swapnil-197 <sspatil1907@gmail.com>
2024-05-24 17:51:44 +0530

    Load Balancer Concept added

--- 07 - SpringBoot_MS_WELCOME_API/src/main/java/com/boot/rest/WelcomeRestController.java
+++ 07 - SpringBoot_MS_WELCOME_API/src/main/java/com/boot/rest/WelcomeRestController.java
@@ -1,14 +1,20 @@
 package com.boot.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeRestController {
 
+	@Autowired
+	private Environment env ;
+	
 	@GetMapping("/welcome")
 	public String getWelcomeMsg() {
-		String msg = "Welcome to SpringBoot & Microservices Concept..!!";
+		String port = env.getProperty("server.port");
+		String msg = "Welcome to Microservices Concept..!! (Server Port :: " + port +")";
 		return msg ;
 	}
 }
